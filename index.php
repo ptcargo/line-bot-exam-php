00c507650f04b70e4bb0bc8d0e8d423b7fdefa87
ptcargo <43976799+ptcargo@users.noreply.github.com>
2018-10-09 09:44:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,9 @@
    $arrayHeader[] = "Content-Type: application/json";
    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
    //รับข้อความจากผู้ใช้
-   //$message = $arrayJIssueson['events'][0]['message']['text'];
+   $message = $arrayJIssueson['events'][0]['message']['text'];
    //รับ id ของผู้ใช้
    $id = $arrayJson['events'][0]['source']['userId'];
-$message = "นับ 1-10";
    if($message == "นับ 1-10"){
        for($i=1;$i<=10;$i++){
           $arrayPostData['to'] = $id;
